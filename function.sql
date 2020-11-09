3a75c2e7adcc8aaed354ffb116a20be9c708f8f7
thaison247 <nguyenthaison247@gmail.com>
2020-11-09 11:25:52 +0700

    function returns a table

--- function.sql
+++ function.sql
@@ -57,3 +57,28 @@ $$;
 
 select * from swap(2, 5);
 
+
+-- FUNCTION RETURN A TABLE
+-- viết function nhận vào length_from và length_to, lấy ds film có length thuộc [length_from, length_to]
+create or replace function get_range_films(
+	length_from int,
+	length_to int
+)
+returns table(title text, film_len int)
+language plpgsql
+as
+$$
+declare
+	it record;
+begin
+	for it in (
+		select film.title, film.length
+		from film
+		where length between length_from and length_to
+	) loop
+		title := upper(it.title);
+		film_len := it.length;
+		return next;
+	end loop;
+end;
+$$;
